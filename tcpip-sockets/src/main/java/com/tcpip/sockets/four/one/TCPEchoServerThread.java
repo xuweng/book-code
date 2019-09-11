730eb2d054cfb4427c212ac03460cc7b8b49c367
xuweng <2028403305@qq.com>
2019-09-11 23:10:35 +0800

    一客户一线程

---
+++ tcpip-sockets/src/main/java/com/tcpip/sockets/four/one/TCPEchoServerThread.java
@@ -0,0 +1,36 @@
+package com.tcpip.sockets.four.one;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+/**
+ * 一客户一线程
+ */
+public class TCPEchoServerThread {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) { // Test for correct # of args
+            throw new IllegalArgumentException("Parameter(s): <Port>");
+        }
+
+        int echoServPort = Integer.parseInt(args[0]); // Server port
+
+        // Create a server socket to accept client connection requests
+        ServerSocket servSock = new ServerSocket(echoServPort);
+
+        Logger logger = Logger.getLogger("practical");
+
+        // Run forever, accepting and spawning a thread for each connection
+        while (true) {
+            Socket clntSock = servSock.accept(); // Block waiting for connection
+            // Spawn thread to handle new connection
+            Thread thread = new Thread(new EchoProtocol(clntSock, logger));
+            thread.start();
+            logger.info("Created and started Thread " + thread.getName());
+        }
+        /* NOT REACHED */
+    }
+}
